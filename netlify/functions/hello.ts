0894f9b8603a2628300108b9830ba0faf4565649
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2021-04-26 15:47:57 +0900

    logging

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,7 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
+  console.log(event);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
