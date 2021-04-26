340d1e3e475e8854c16b8038208e0fda2e9a9fe0
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2021-04-26 16:04:30 +0900

    type param

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,10 +1,25 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
-  console.log(event);
+  console.log(event.queryStringParameters);
+  if (event.queryStringParameters === null) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World" }),
+    };
+  }
+
+  const typ = event.queryStringParameters['type'] || 'json';
+  if (typ === 'json') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "json" }),
+    };
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "default" }),
   };
 };
 
